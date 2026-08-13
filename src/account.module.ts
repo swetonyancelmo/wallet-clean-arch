5d5164ce5c3476e61b6d1c1dcf715a05ac509712
Swetony Ancelmo <swetonyancelmo@gmail.com>
2026-08-13 00:11:29 -0300

    feat: adiciona account module e configura conexão com postgres

---
+++ src/account.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountOrmEntity } from './infrastructure/database/typeorm/entities/account.orm-entity';
+import { CreateAccountUseCase } from './application/account/use-cases/create-account.use-case';
+import { DepositUseCase } from './application/account/use-cases/deposit.use-case';
+import { WithdrawUseCase } from './application/account/use-cases/withdraw.use-case';
+import { TransferUseCase } from './application/account/use-cases/transfer.use-case';
+import { ACCOUNT_REPOSITORY } from './domain/account/repositories/account.repository';
+import { TypeOrmAccountRepository } from './infrastructure/database/typeorm/repositories/typeorm-account.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AccountOrmEntity])],
+  providers: [
+    CreateAccountUseCase,
+    DepositUseCase,
+    WithdrawUseCase,
+    TransferUseCase,
+    {
+      provide: ACCOUNT_REPOSITORY,
+      useClass: TypeOrmAccountRepository,
+    },
+  ],
+  exports: [
+    CreateAccountUseCase,
+    DepositUseCase,
+    WithdrawUseCase,
+    TransferUseCase,
+  ],
+})
+export class AccountModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountModule } from './account.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    AccountModule,
+  ],
   controllers: [],
   providers: [],
 })
