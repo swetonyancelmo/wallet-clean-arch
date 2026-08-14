f814585aecb4577ca08ccfae10d60c22f7f71127
Swetony Ancelmo <swetonyancelmo@gmail.com>
2026-08-14 15:53:26 -0300

    feat: adiciona camada de controller

--- src/account.module.ts
+++ src/account.module.ts
@@ -7,6 +7,7 @@ import { WithdrawUseCase } from './application/account/use-cases/withdraw.use-ca
 import { TransferUseCase } from './application/account/use-cases/transfer.use-case';
 import { ACCOUNT_REPOSITORY } from './domain/account/repositories/account.repository';
 import { TypeOrmAccountRepository } from './infrastructure/database/typeorm/repositories/typeorm-account.repository';
+import { AccountController } from './presentation/account/controllers/account.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AccountOrmEntity])],
@@ -26,5 +27,6 @@ import { TypeOrmAccountRepository } from './infrastructure/database/typeorm/repo
     WithdrawUseCase,
     TransferUseCase,
   ],
+  controllers: [AccountController],
 })
 export class AccountModule {}
---
+++ src/presentation/account/controllers/account.controller.ts
@@ -0,0 +1,49 @@
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CreateAccountUseCase } from 'src/application/account/use-cases/create-account.use-case';
+import { DepositUseCase } from 'src/application/account/use-cases/deposit.use-case';
+import { TransferUseCase } from 'src/application/account/use-cases/transfer.use-case';
+import { WithdrawUseCase } from 'src/application/account/use-cases/withdraw.use-case';
+import { CreateAccountDto } from '../dtos/create-account.dto';
+import { DepositDto } from '../dtos/deposit.dto';
+import { WithdrawDto } from '../dtos/withdraw.dto';
+import { TransferDto } from '../dtos/transfer.dto';
+
+@Controller('accounts')
+export class AccountController {
+  constructor(
+    private readonly createAccountUseCase: CreateAccountUseCase,
+    private readonly depositUseCase: DepositUseCase,
+    private readonly withdrawUseCase: WithdrawUseCase,
+    private readonly transferUseCase: TransferUseCase,
+  ) {}
+
+  @Post()
+  create(@Body() dto: CreateAccountDto) {
+    return this.createAccountUseCase.execute({ ownerName: dto.ownerName });
+  }
+
+  @Post(':id/deposit')
+  deposit(@Param('id') id: string, @Body() dto: DepositDto) {
+    return this.depositUseCase.execute({
+      accountId: id,
+      amountInCents: dto.amountInCents,
+    });
+  }
+
+  @Post(':id/withdraw')
+  withdraw(@Param('id') id: string, @Body() dto: WithdrawDto) {
+    return this.withdrawUseCase.execute({
+      accountId: id,
+      amountInCents: dto.amountInCents,
+    });
+  }
+
+  @Post(':id/transfer')
+  transfer(@Param('id') fromAccountId: string, @Body() dto: TransferDto) {
+    return this.transferUseCase.execute({
+      amountInCents: dto.amountInCents,
+      fromAccountId: fromAccountId,
+      toAccountId: dto.toAccountId,
+    });
+  }
+}
